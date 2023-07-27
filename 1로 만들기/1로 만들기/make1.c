82b2ce74825fd8c4fdb995d1479c344b4dc0101c
5AgA <silvera03@naver.com>
2023-07-27 12:45:00 +0900

    7/27-3

--- 1로 만들기/1로 만들기/make1.c
+++ 1로 만들기/1로 만들기/make1.c
@@ -3,24 +3,23 @@
 
 int main(void)
 {
-	int X, i = 2;
+	int X;
 	int M[1000001] = {0};
 
 	scanf("%d", &X);
 
 	M[1] = 0;
 
-	while (i <= X) {
+	for (int i = 2; i <= X; i++) {
+		M[i] = M[i - 1] + 1;
 		if (i % 3 == 0) {
-			M[i] = 1 + M[i / 3];
+			if (M[i] > M[i / 3] + 1)
+				M[i] = M[i / 3] + 1;
 		}
-		else if ((i - 1) % 3 == 0) {
-			M[i] = 2 + M[(i - 1) / 3];
+		if (i % 2 == 0) {
+			if (M[i] > M[i / 2] + 1)
+				M[i] = M[i / 2] + 1;
 		}
-		else if (i % 2 == 0) {
-			M[i] = 1 + M[i / 2];
-		}
-		i++;
 	}
 
 	printf("%d\n", M[X]);
